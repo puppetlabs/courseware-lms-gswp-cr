b7cca33981406c2bf8fcaa465459dbf22fd208b6
Matt Cadorette <mcadorette@gmail.com>
2018-09-05 13:21:42 -0400

    Delete ssh_config.pp

--- site/profile/manifests/baseline/linux/ssh_config.pp
+++
@@ -1,9 +0,0 @@
-class profile::baseline::linux::ssh_config {
-
-    class{ '::ssh':
-      permit_root_login            => 'no',
-      sshd_password_authentication => 'no',
-      sshd_client_alive_interval   => '3600',
-      sshd_client_alive_count_max  => '0',
-    }
-}
